323afdef49a405e1a7bdbc648e3fb3ab1b532838
saseuz <soulkeeper94@gmail.com>
2025-10-25 18:11:51 +0700

    adding recipe feature

--- database/migrations/2025_10_23_095538_create_recipes_table.php
+++ database/migrations/2025_10_23_095538_create_recipes_table.php
@@ -24,6 +24,8 @@ return new class extends Migration
             $table->string('thumbnail')->nullable();
             $table->enum('status', ['draft', 'published']);
             $table->foreignId('user_id')->constrained()->onDelete('cascade'); // user_id
+            $table->uuid('cuisine_id')->nullable();
+            $table->foreign('cuisine_id')->references('id')->on('cuisines')->onDelete('cascade');
             $table->timestamps();
         });
     }

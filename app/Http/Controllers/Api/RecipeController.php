3792b56dfa50acd8b515b2ac18dad5f4c51aa245
saseuz <soulkeeper94@gmail.com>
2025-11-04 17:50:03 +0700

    add new methods for crud

--- app/Http/Controllers/Api/RecipeController.php
+++ app/Http/Controllers/Api/RecipeController.php
@@ -6,13 +6,14 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\RecipeApiRequest;
 use App\Models\Backend\Recipe;
 use App\Traits\HasImageUpload;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class RecipeController extends Controller
 {
     use HasImageUpload;
     
-    public function list()
+    public function list(): JsonResponse
     {
         $recipes = Recipe::latest()->paginate();
 
@@ -21,7 +22,19 @@ class RecipeController extends Controller
         ]);
     }
 
-    public function store(RecipeApiRequest $request)
+    public function myRecipes(): JsonResponse
+    {
+        $recipes = Recipe::where('user_id', auth()->user()->id)->latest()->paginate();
+
+        return response()->json([
+            'status' => 'success',
+            'response_code' => 200,
+            'message' => 'My recipes retrieved successfully',
+            'data' => $recipes,
+        ], 200);
+    }
+
+    public function store(RecipeApiRequest $request): JsonResponse
     {
         $validatedData = $request->validated();
         $validatedData['user_id'] = $request->user()->id;
@@ -47,7 +60,7 @@ class RecipeController extends Controller
         ], 201);
     }
 
-    public function detail(string $slug)
+    public function detail(string $slug): JsonResponse
     {
         $recipe = Recipe::where('slug', $slug)->first();
 
@@ -57,5 +70,72 @@ class RecipeController extends Controller
             'message' => 'Recipe detail info.',
             'data' => $recipe,
         ], 200);
+    }
+
+    public function update(RecipeApiRequest $request, string $slug): JsonResponse
+    {
+        $recipe = Recipe::where('slug', $slug)->firstOrFail();
+        $validatedData = $request->validated();
+
+        if ($recipe->user_id !== auth()->user()->id) {
+            return response()->json([
+                'status' => 'error',
+                'response_code' => 403,
+                'message' => 'You are not authorized to update this recipe.',
+                'data' => $recipe,
+            ], 403);
+        }
+
+        if ($request->hasFile('image')) {
+            // Delete old images
+            $this->deleteOldImage($recipe->thumbnail, 'recipes/thumbnails');
+            $this->deleteOldImage($recipe->image, 'recipes');
+
+            // Thumbnail Image
+            $validatedData['thumbnail'] = $this->saveImage($request->file('image'), 'recipes/thumbnails', 800, 600);
+
+            // Cover Image
+            $validatedData['image'] = $this->saveImage($request->file('image'), 'recipes', 1200, 800);
+        }
+
+        $recipe->update($validatedData);
+        if (isset($validatedData['categories'])) {
+            $recipe->categories()->sync($validatedData['categories']);
+        }
+
+        if (isset($validatedData['ingredients'])) {
+            $recipe->ingredients()->delete();
+            $recipe->ingredients()->createMany($validatedData['ingredients']);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'response_code' => 200,
+            'message' => 'Recipe updated successfully.',
+            'data' => $recipe,
+        ], 200);
+    }
+
+    public function destroy(string $slug): JsonResponse
+    {
+        $recipe = Recipe::where('slug', $slug)->firstOrFail();
+
+        if ($recipe->user_id !== auth()->user()->id) {
+            return response()->json([
+                'status' => 'error',
+                'response_code' => 403,
+                'message' => 'You are not authorized to update this recipe.',
+                'data' => $recipe,
+            ], 403);
+        }
+
+        $recipe->delete();
+
+        return response()->json([
+            'status' => 'success',
+            'response_code' => 204,
+            'message' => 'Recipe deleted successfully.',
+            'data' => $recipe,
+        ], 204);
     }
 }

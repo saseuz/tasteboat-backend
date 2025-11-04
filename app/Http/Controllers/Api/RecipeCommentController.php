cc81d99dab737a1b50012a108680c6b8084b76f7
saseuz <soulkeeper94@gmail.com>
2025-11-04 17:59:20 +0700

    added update/delete methods

--- app/Http/Controllers/Api/RecipeCommentController.php
+++ app/Http/Controllers/Api/RecipeCommentController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Backend\Comment;
 use App\Models\Backend\Recipe;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class RecipeCommentController extends Controller
 {
-    public function store(string $slug, Request $request)
+    public function store(string $slug, Request $request): JsonResponse
     {
         $request->validate([
             'content' => 'required|string|max:1000',
@@ -29,5 +31,51 @@ class RecipeCommentController extends Controller
             'message' => 'Comment submitted successfully.',
             'data' => $comment,
         ], 201);
+    }
+
+    public function update(Comment $comment, Request $request)
+    {
+        $validatedData = $request->validate([
+            'content' => 'required|string|max:1000',
+        ]);
+
+        if ($comment->user_id !== auth()->user()->id) {
+            return response()->json([
+                'status' => 'error',
+                'response_code' => 403,
+                'message' => 'You are not authorized to update this comment.',
+                'data' => $comment,
+            ], 403);
+        }
+
+        $comment->update($validatedData);
+
+        return response()->json([
+            'status' => 'success',
+            'response_code' => 200,
+            'message' => 'Comment updated successfully.',
+            'data' => $comment,
+        ], 200);
+    }
+
+    public function destroy(Comment $comment): JsonResponse
+    {
+        if ($comment->user_id !== auth()->user()->id) {
+            return response()->json([
+                'status' => 'error',
+                'response_code' => 403,
+                'message' => 'You are not authorized to delete this comment.',
+                'data' => $comment,
+            ], 403);
+        }
+
+        $comment->delete();
+
+        return response()->json([
+            'status' => 'success',
+            'response_code' => 204,
+            'message' => 'Comment deleted successfully.',
+            'data' => $comment,
+        ], 204);
     }
 }
